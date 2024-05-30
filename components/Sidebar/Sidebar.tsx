d44ec7aa2a617cdc254c70400c2ab8165c0560d0
Eshika rawat <eshikarawat02@gmail.com>
2024-05-30 09:38:35 +0530

    fixed bugs

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,35 +1,25 @@
-"use client";
+'use client';
 
-import React from "react";
-import Link from "next/link";
-
-
+import React from 'react';
+import Link from 'next/link';
 
 const Sidebar = () => {
-
-
-  return (
-    <section
-      className="sticky left-0 top-0 flex h-screen w-fit text-white flex-col justify-between bg-dark-1 p-6 pt-28
-    max-sm:hidden lg:w-[264px]"
-    >
-      <div className="flex flex-col flex-1 gap-6">
-        {["home", "contact us"].map((link) => {
-
-          return (
-            <Link
-              href={"/"}
-              key={link}
-            >
-              <p className="text-lg font-semibold max-lg:hidden">
-                {link}
-              </p>
-            </Link>
-          );
-        })}
-      </div>
-    </section>
-  );
+    return (
+        <section
+            className='bg-dark-1 sticky left-0 top-0 flex h-screen w-fit flex-col justify-between p-6 pt-28 text-white
+    max-sm:hidden lg:w-[264px]'
+        >
+            <div className='flex flex-1 flex-col gap-6'>
+                {['home', 'contact us'].map((link) => {
+                    return (
+                        <Link href={'/'} key={link}>
+                            <p className='text-lg font-semibold max-lg:hidden'>{link}</p>
+                        </Link>
+                    );
+                })}
+            </div>
+        </section>
+    );
 };
 
 export default Sidebar;
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,20 +1,18 @@
-import React, { ReactNode } from 'react'
-import Sidebar from '../Sidebar/Sidebar'
-import { Flex } from '../flex'
+import React, { ReactNode } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import { Flex } from '../flex';
 
-const MainLayout = ({ children }: {children : ReactNode}) => {
-  return (
-    <main>
-      <Flex dir='row' as={"div"}>
-        <Sidebar />
-        <section className="flex min-h-screen flex-col flex-1 px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
-            <div className="w-full">
-                {children}
-            </div>
-        </section>
-      </Flex>
-    </main>
-  )
-}
+const MainLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <main>
+            <Flex dir='row' as={'div'}>
+                <Sidebar />
+                <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+                    <div className='w-full'>{children}</div>
+                </section>
+            </Flex>
+        </main>
+    );
+};
 
-export default MainLayout
+export default MainLayout;
