1fbfe89f9921826b25412f075a85285bcb44911a
naveensgs <naveen@emoment.tech>
2024-05-31 18:19:20 +0530

    DS001: Intialized next project.

--- components/flex/index.ts
+++
@@ -1,4 +0,0 @@
-export { Flex } from './flex';
-export { FlexItem } from './flex-item';
-export { flexVariants, flexItemVariants } from './flex.variants';
-export type { FlexProps, FlexItemProps } from './flex.variants';
