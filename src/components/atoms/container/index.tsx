e7099538765221892d6fba4825616cf87446f65c
naveensgs <naveen@emoment.tech>
2024-06-01 18:38:50 +0530

    Added container component.

---
+++ src/components/atoms/container/index.tsx
@@ -0,0 +1,14 @@
+import { twMerge } from "tailwind-merge";
+
+const Container: React.FC<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+> = ({ children, className, ...props }) => {
+  const style = twMerge(["container mx-auto", className]);
+  return (
+    <div className={style} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
