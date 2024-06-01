6c4f5f17ec7c757f05d5fc2f048c30ba243d2470
naveensgs <naveen@emoment.tech>
2024-06-01 07:51:00 +0530

    Updated storybook version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
     stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
     addons: [
         '@storybook/addon-onboarding',
         '@storybook/addon-links',
@@ -9,9 +10,16 @@ const config: StorybookConfig = {
         '@chromatic-com/storybook',
         '@storybook/addon-interactions',
     ],
+
     framework: {
         name: '@storybook/nextjs',
         options: {},
     },
+
+    docs: {},
+
+    typescript: {
+        reactDocgen: 'react-docgen-typescript'
+    }
 };
 export default config;
