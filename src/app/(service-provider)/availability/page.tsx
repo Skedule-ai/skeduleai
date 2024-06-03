2bf72f172a324576d372ce613317eb72a2d4f7a0
mukesh-emt <mukesh@emoment.in>
2024-06-04 02:46:46 +0530

    model component implementation and form validation done

---
+++ src/app/(service-provider)/availability/page.tsx
@@ -0,0 +1,5 @@
+import Modal1 from '@/components/molecules/modal1';
+
+export default function AvailabilityPage() {
+    return <Modal1 />;
+}
---
+++ src/components/organisms/availability-form/availability-form-validation.ts
@@ -0,0 +1,9 @@
+import * as Yup from 'yup';
+
+const availabilityFormValidation  = Yup.object().shape({
+    businessHours: Yup.string().required('Business hours are required'),
+    numberOfSlots: Yup.number().required('Number of slots is required').min(1, 'At least one slot is required'),
+    businessDays: Yup.array().of(Yup.string()).required('At least one business day is required'),
+});
+
+export default availabilityFormValidation ;
