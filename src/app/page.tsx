78927c368ac6a8e9d153eb284693b6ed3e7c2540
naveensgs <naveen@emoment.tech>
2024-06-05 00:19:52 +0530

    SKEDU-25: Implementation of header component for landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import AuthHeader from '@/components/organisms/auth-header';
+import LandingPageHeader from '@/components/organisms/headers/landingpage-header';
 
 export default function Home() {
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <AuthHeader />
+        <main>
+            <LandingPageHeader />
         </main>
     );
 }
