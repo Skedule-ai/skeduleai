b3f27dbae94ddde256d1a81451cb769abf9cfb75
Mukesh Sahu <mukesh2sahu786@gmail.com>
2024-06-04 19:00:23 +0530

    Createed the booking  Page header

---
+++ src/stories/atoms/PageHeader.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PageHeader from '../../components/atoms/pageheader/index';
+const meta: Meta<typeof PageHeader> = {
+    title: 'Atoms/Header',
+    component: PageHeader,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        logoSrc: 'https://via.placeholder.com/150',
+        logoAlt: 'Logo',
+        OrganizationName: 'Organization Name',
+    },
+};
