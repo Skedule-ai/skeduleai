eecda7df639ffa342484b369cb1fb39d35befeb8
= <=>
2024-06-06 16:48:12 +0530

    SKEDU-41:Adding the screen 2 modal in storybook

---
+++ src/stories/atoms/booking-modal.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import BookingModalVariants from '@/components/atoms/modals/booking-modal-variants';
+import { fireEvent, within } from '@storybook/test';
+
+const meta: Meta<typeof BookingModalVariants> = {
+    title: 'Atoms/BookingModal',
+    component: BookingModalVariants,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {},
+    args: {
+        onSubmit: action('submitted'),
+    },
+} satisfies Meta<typeof BookingModalVariants>;
+
+export default meta;
+type Story = StoryObj<typeof BookingModalVariants>;
+
+export const Default: Story = {
+    args: {},
+};
+
+export const SignInFormActive: Story = {
+    args: {},
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+        const signInTab = await canvas.findByText('Sign In');
+        fireEvent.click(signInTab);
+    },
+};
