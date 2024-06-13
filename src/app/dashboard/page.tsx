087663cdd3116466cffd6f0a455ef82b979d8ba0
Utkal Nayak <utkal.nayak@emoment.in>
2024-06-13 10:28:20 +0530

    Add Accepet Reject card on Dashboard Page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,13 +3,55 @@ import React from 'react';
 import Date from '@/components/atoms/date';
 import SideBar from '@/components/organisms/sidebar';
 import { Flex } from '@/components/atoms/flex';
+import Container from '@/components/atoms/container'; // Assuming you have a Container component
+import AcceptRejectCard from '@/components/atoms/card/AcceptRejectCard';
+import Grid from '@/components/atoms/grid';
 
 const Home = () => {
     return (
         <>
             <Flex>
                 <SideBar />
-                <Date />
+                <Container>
+                    <Date />
+
+                    <Grid columns={3} gap={2} rows={3}>
+                        <React.Fragment key='.0'>
+                            {/* <span className='size-10 bg-neutral-950'>1</span> */}
+                            <AcceptRejectCard
+                                fromTime='12:30 PM'
+                                isFree
+                                onAccept={() => {}}
+                                onReject={() => {}}
+                                size='lg'
+                                title='Brainstorming session'
+                                toTime='04:36 PM IST'
+                                userImages={[
+                                    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+                                ]}
+                                variant='default'
+                            >
+                                <></>
+                            </AcceptRejectCard>
+                            <AcceptRejectCard
+                                className='ml-10'
+                                fromTime='12:30 PM'
+                                isFree
+                                onAccept={() => {}}
+                                onReject={() => {}}
+                                size='lg'
+                                title='Brainstorming session'
+                                toTime='04:36 PM IST'
+                                userImages={[
+                                    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+                                ]}
+                                variant='default'
+                            >
+                                <></>
+                            </AcceptRejectCard>
+                        </React.Fragment>
+                    </Grid>
+                </Container>
             </Flex>
         </>
     );
--- src/components/atoms/card/AcceptRejectCard.tsx
+++ src/components/atoms/card/AcceptRejectCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './index';
 import { CardProps } from './card.variants';
 import { Flex } from '../flex';
-// import { Clock } from '@strapi/icons';
+import { Clock } from '@strapi/icons';
 import Image from 'next/image';
 
 type AcceptRejectCardProps = {
@@ -17,9 +17,9 @@ type AcceptRejectCardProps = {
 
 const AcceptRejectCard: React.FC<AcceptRejectCardProps> = ({
     title,
-    // fromTime,
-    // toTime,
-    // isFree,
+    fromTime,
+    toTime,
+    isFree,
     userImages,
     onAccept,
     onReject,
@@ -34,7 +34,16 @@ const AcceptRejectCard: React.FC<AcceptRejectCardProps> = ({
                     <Flex dir='row' justifyContent='end' className='mt-2'>
                         <Flex dir='row' className='relative'>
                             {userImages.slice(0, 5).map((image, index) => (
-                                <Image
+                                // <Image
+                                //     key={index}
+                                //     src={image}
+                                //     alt={`User ${index + 1}`}
+                                //     className={`size-5 rounded-full object-cover md:size-6 ${index !== 0 && '-ml-2'}`}
+                                //     style={{ zIndex: 5 - index }}
+                                //     width={50}
+                                //     height={50}
+                                // />
+                                <img
                                     key={index}
                                     src={image}
                                     alt={`User ${index + 1}`}
@@ -54,13 +63,13 @@ const AcceptRejectCard: React.FC<AcceptRejectCardProps> = ({
                     gap={1}
                     className='mt-4 text-xs font-medium text-gray-500 md:text-sm'
                 >
-                    {/* <Clock className='flex size-4 items-center justify-center' />
+                    <Clock className='flex size-4 items-center justify-center' />
                     <p className='flex items-center gap-1'>
                         <span className='text-green-600'>{fromTime}</span>
                         <span className='text-gray-500'>{'->'}</span>
                         <span className='text-red-500'>{toTime}</span>
                         {isFree && <span className='text-blue-500'>(Free)</span>}
-                    </p> */}
+                    </p>
                 </Flex>
 
                 <hr className='my-4' />
