37ca3f0d66575a21ae8fb6d73cb0a37187deeab6
Sindhu <lsindhuBTECH21@ced.alliance.edu.in>
2024-06-13 16:19:01 +0530

    created an api for accpet and reject

--- src/backend/repositories/appointmentRepository.ts
+++ src/backend/repositories/appointmentRepository.ts
@@ -1,6 +1,22 @@
 import { prisma } from '@/backend/utils/db';
 import { BookingDetailsDTO } from '../interfaces/bookingServiceDTO';
+import { AppointmentStatus } from '../utils/enum';
 
 export async function createAppoinmentRepository(data: BookingDetailsDTO) {
     return await prisma.bookingDetails.create({ data });
+}
+
+export async function findBookingDetails(id: string) {
+    // Find the booking service
+    return await prisma.bookingDetails.findFirst({
+        where: { id, status: AppointmentStatus.PENDING },
+    });
+}
+
+export async function updateBookingStatusRepo(id: string, status: AppointmentStatus) {
+    // Find the booking service
+    return await prisma.bookingDetails.update({
+        where: { id },
+        data: { status }, // Include related bookingDetails
+    });
 }
