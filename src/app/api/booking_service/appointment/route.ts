5a75db8f8f18f832a86db273ff44b51bdd047257
Sindhu <lsindhuBTECH21@ced.alliance.edu.in>
2024-06-27 13:11:52 +0530

    SKEDU-106: fetch appintment api based on date

--- src/app/api/booking_service/appointment/route.ts
+++ src/app/api/booking_service/appointment/route.ts
@@ -1,11 +1,23 @@
-import { NextResponse } from 'next/server';
 import { findAppointmentsController } from '@/backend/controllers/appointmentController';
+import moment from 'moment';
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url);
-        const appointment = await findAppointmentsController(searchParams?.get('id'));
-        return NextResponse.json(appointment, { status: 201 });
+        console.log(searchParams);
+        const organizationId = searchParams?.get('id');
+        const dateParam = searchParams?.get('date');
+
+        let date: Date | string;
+        if (dateParam) {
+            date = moment(dateParam, 'DD-MM-YYYY').toISOString();
+        } else {
+            date = new Date().toISOString(); // Default to today's date if no date param provided
+        }
+
+        const appointments = await findAppointmentsController(organizationId, date);
+        return NextResponse.json(appointments, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 400 });
     }
